7e9e70e5121e692b6f78b8e7230c9b4c07db9843
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-03-20 11:27:13 +0100

    created new branch with task9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        String[] arrayCities = {"paris", "london", "madrid"};
+        for (int i = 0; i < arrayCities.length; i++) {
+            arrayCities[i] = arrayCities[i].replace(arrayCities[i].charAt(0), (char) (arrayCities[i].charAt(0) - 32));
+        }
+        System.out.println(Arrays.toString(arrayCities));
     }
 }
