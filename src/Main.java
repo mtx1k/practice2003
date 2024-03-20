d59fd42c7b6951316cc61471c8a7598d0391a2cc
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-03-20 11:58:44 +0100

    created new branch with task12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(Arrays.toString(repeatItem("cool", 3)));
+    }
+    private static String[] repeatItem(String str, int count) {
+        if (count == 0) {
+            return null;
+        }
+        String[] result = new String[count];
+        Arrays.fill(result, str);
+        return result;
     }
 }
