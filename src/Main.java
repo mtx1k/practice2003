f581312935321d5292e6767108e1f2844811840d
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-03-20 13:56:21 +0100

    created new branch with task14

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(reverse("myself"));
+    }
+    private static String reverse(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--) {
+            result.append(str.charAt(i)).append("\n");
+        }
+        return result.toString();
     }
 }
