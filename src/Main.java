9b9b95854a31d7bfeaa39036faf580d7eb9672b8
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-03-20 10:56:36 +0100

    created new branch with task4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        for (int i = 1; i < 5; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
     }
 }
