85b91e84400991f81b05dfc5d712f59834138ed2
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-03-20 11:34:50 +0100

    created new branch with task10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        String string = "something";
+        char[] charsFromString = string.toCharArray();
+        for (int i = 0; i < charsFromString.length; i += 2) {
+            charsFromString[i] = (char) (charsFromString[i] - 32);
+        }
+        String resultString = new String(charsFromString);
+        System.out.println(string + " -> " + resultString);
     }
 }
