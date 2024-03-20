e63f9a56be8db15fe85feb061f31ed20782aeb1f
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-03-20 11:12:13 +0100

    created new branch with task8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-
+        for (int i = 1; i <= 4; i++) {
+            System.out.println("There " + ((i == 1) ? "is " : "are ") + i + " cup on the table.");
+        }
     }
 }
