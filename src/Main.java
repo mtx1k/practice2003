50e954bf582be3a776a71b022a4d56e38273f5d7
Vitalii <vitaliy.shekhovtsov@gmail.com>
2024-03-20 11:51:26 +0100

    created new branch with task 11

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-
+        String name1 = "Ali";
+        String name2 = "Nancy";
+        System.out.println(name1 + " -> " + vovelCounter(name1));
+        System.out.println(name2 + " -> " + vovelCounter(name2));
+    }
+    private static int vovelCounter(String str) {
+        return str.replaceAll("[^aeiouAEIOU]+", "").length();
     }
 }
